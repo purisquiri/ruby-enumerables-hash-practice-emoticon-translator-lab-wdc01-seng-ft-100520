75d92321c7f26265a64a9147d8a0a04e6c25498c
Mauro Dorigo <mau.dorigo@outlook.es>
2020-09-14 15:30:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,10 +21,10 @@ def get_japanese_emoticon(file_path, emoticon)
   updated_hash = load_library(file_path)
   
   updated_hash.each do |emoticon_obj|
-    #binding.pry
+    binding.pry
     if emoticon_obj[1][:english] == emoticon
       return emoticon_obj[1][:japanese]
-    binding.pry
+    #binding.pry
     else
       return "Sorry, that emoticon was not found"  
     end
